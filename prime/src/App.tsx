1c61bfd6e7db76fc5b1a8036bdc43b1c2f85289e
Dom Grad <Dominikgrad47@gmail.com>
2023-08-19 13:42:51 +0100

    aa

--- prime/src/App.tsx
+++ prime/src/App.tsx
@@ -1,6 +1,24 @@
+import { Calendar } from "primereact/calendar";
+import { useState } from "react";
+import { Checkbox } from "primereact/checkbox";
+
 function App() {
+  const [dates, setDates] = useState(null);
+  const [checked, setChecked] = useState(false);
+
   return (
-    <div className="bg-slate-400 min-h-screen w-full text-white flex items-center justify-center flex-col gap-y-5"></div>
+    <div className="bg-slate-400 min-h-screen w-full text-white flex items-center justify-center flex-col gap-y-5">
+      <Calendar
+        value={dates}
+        onChange={(e) => setDates(e.value)}
+        selectionMode="range"
+      />
+      <Checkbox
+        icon={<div className="bg-yellow-500 w-2 h-2 rounded"></div>}
+        onChange={(e) => setChecked(e.checked)}
+        checked={checked}
+      ></Checkbox>
+    </div>
   );
 }
 
--- prime/src/main.tsx
+++ prime/src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
