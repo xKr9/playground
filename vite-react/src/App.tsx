5ebb806769508118a0ac25d1af03fdf04b2ef0b0
Dom Grad <Dominikgrad47@gmail.com>
2024-02-12 18:38:11 +0000

    added vite init with new libs

---
+++ vite-react/src/App.tsx
@@ -0,0 +1,37 @@
+import { Suspense } from "react";
+import { useQuery } from "react-query";
+import { ErrorBoundary } from "react-error-boundary";
+
+const examplePromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject("This is an error");
+  }, 1000);
+});
+
+function App() {
+  return (
+    <main className="min-h-screen w-full flex flex-col bg-blue-300 text-white">
+      <ErrorBoundary fallback={<div>Shit</div>}>
+        <Suspense fallback={<div>This is Loading....</div>}>
+          <Example />
+        </Suspense>
+      </ErrorBoundary>
+    </main>
+  );
+}
+
+const Example = () => {
+  const { data } = useQuery({
+    queryKey: "example",
+    queryFn: () => examplePromise,
+    suspense: true,
+    useErrorBoundary: true,
+  });
+  return (
+    <ErrorBoundary fallback={<div>Inner shit</div>}>
+      <h1>{data}</h1>
+    </ErrorBoundary>
+  );
+};
+
+export default App;
