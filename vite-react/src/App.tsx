119c90f7ff224ca7a203dbe37f2e456d28eb48c2
Dom Grad <Dominikgrad47@gmail.com>
2024-04-03 11:28:14 +0100

    adds blur test

--- vite-react/src/App.tsx
+++ vite-react/src/App.tsx
@@ -1,15 +1,25 @@
 import Card from "./components/MultiComponentThing";
+import PlaceholderImg from "@/assets/image.webp";
 
 function App() {
   return (
-    <main className="min-h-screen w-full flex flex-col bg-blue-300 text-white">
-      <Card>
+    <main className="min-h-screen w-full flex justify-center items-center flex-col bg-blue-300 text-white">
+      {/* <Card>
         <Card.Title title="Hello"></Card.Title>
         <Card.Body>
           <div className="flex justify-center">Some Random Content</div>
         </Card.Body>
         <Card.Footer description="Whats up"></Card.Footer>
-      </Card>
+      </Card> */}
+      <img
+        width={500}
+        style={{
+          backgroundImage: PlaceholderImg,
+        }}
+        height={500}
+        // src="https://images.unsplash.com/photo-1525338078858-d762b5e32f2c?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="ai"
+      />
     </main>
   );
 }
